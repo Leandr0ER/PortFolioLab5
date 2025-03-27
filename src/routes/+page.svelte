9755d0bffdafdac87a390679dfc30db0df56fbb2
Leighton Estrada <lestradar@uni.edu.pe>
2025-03-27 12:42:19 -0300

    Lab5 upd4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,12 @@
 <script>
   import projects from "$lib/projects.json";
   import Project from "$lib/Project.svelte";
+
+  let profileData = fetch("https://api.github.com/users/Leandr0ER");
 </script>
-<h1> Leiton Leandro Estrada R.</h1>
+<h1> Leiton Estrada R.</h1>
    
-<img src="images/Imagen001.jpg" alt="mike" width="200px">
+<img src="images/Imagen001.jpg" alt="mike" width="300px">
 
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 </p>
